78dea28d5305872b17b63862824e3d4e43f691ec
milisaur <milisaur@proton.me>
2026-04-10 17:39:31 +0200

    fix uniraptor mess

--- modules/sddm-wayland.nix
+++
@@ -1,7 +0,0 @@
-{
-  config,
-  pkgs,
-  ...
-}: {
-  services.displayManager.sddm.wayland.enable = true;
-}
--- modules/sddm.nix
+++
@@ -1,9 +0,0 @@
-{
-  config,
-  pkgs,
-  ...
-}: {
-  services.xserver.enable = true;
-
-  services.displayManager.sddm.enable = true;
-}
