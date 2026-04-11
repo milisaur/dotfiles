70834d099968efb0267e30efe87537c9d334f0e6
milisaur <milisaur@proton.me>
2026-04-11 18:06:03 +0200

    mako.nx into uniraptor

--- hosts/uniraptor/home-manager/home.nix
+++ hosts/uniraptor/home-manager/home.nix
@@ -6,6 +6,8 @@
     ../../../modules/home/hyprland/base.nix
     ../../../modules/home/waybar.nix
     ../../../modules/home/hyprland/wallpaper.nix
+    ../../../modules/home/hyprland/lock.nix
+    ../../../modules/home/hyprland/idle.nix
     ../../../modules/home/zsh.nix
     ../../../modules/home/kitty.nix
     ../../../modules/home/fonts.nix
