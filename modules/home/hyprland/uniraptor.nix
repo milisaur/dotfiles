ee68ab7eae7a99d5e384978aa57aaf89f42a6fc4
milisaur <milisaur@proton.me>
2026-04-12 19:39:33 +0200

    bindel and bindl uniraptor add

--- modules/home/hyprland/uniraptor.nix
+++ modules/home/hyprland/uniraptor.nix
@@ -1,18 +1,35 @@
-{ config, pkgs, ... }:
 {
-wayland.windowManager.hyprland = {
-  enable = true;
+  config,
+  pkgs,
+  ...
+}: {
+  wayland.windowManager.hyprland = {
+    enable = true;
 
-  settings = {
-    monitor = [
-    ", preferred, auto, 1"
+    settings = {
+      monitor = [
+        ", preferred, auto, 1"
+      ];
+
+      input = {
+        kb_layout = "de";
+        follow_mouse = 1;
+      };
+    };
+    bindel = [
+      ",XF86AudioRaiseVolume, exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"
+      ",XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+      ",XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+      ",XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
+      ",XF86MonBrightnessUp, exec, brightnessctl -e4 -n2 set 5%+"
+      ",XF86MonBrightnessDown, exec, brightnessctl -e4 -n2 set 5%-"
     ];
 
-    input = {
-      kb_layout = "de";
-      follow_mouse = 1;
-    };
-
-   };
+    bindl = [
+      ",XF86AudioNext, exec, playerctl next"
+      ",XF86AudioPause, exec, playerctl play-pause"
+      ",XF86AudioPlay, exec, playerctl play-pause"
+      ",XF86AudioPrev, exec, playerctl previous"
+    ];
   };
- }
+}
